674aae5757bf653072e895d3f94e69fde88c6478
Guilherme <guicseleg@gmail.com>
2024-08-12 19:15:40 -0300

    Adicionei a qualificação de idade

--- Codigos-teste/teste.py
+++ Codigos-teste/teste.py
@@ -1,2 +1,7 @@
 nome = input('Digite seu nome: ')
+idade = int(input('Quantos anos você tem? '))
 print(f'Que nome bonito o seu {nome}')
+if idade >= 18:
+    print('Você já é maior de idade.')
+else:
+    print('Você ainda é menor de idade.')
