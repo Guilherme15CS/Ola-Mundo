ef8ac67a9030c836067d97542e51c1f545e99dc5
Guilherme <guicseleg@gmail.com>
2024-08-12 21:48:11 -0300

    Atualização trabalho

--- Codigos-teste/teste.py
+++ Codigos-teste/teste.py
@@ -3,5 +3,10 @@ idade = int(input('Quantos anos você tem? '))
 print(f'Que nome bonito o seu {nome}')
 if idade >= 18:
     print('Você já é maior de idade.')
+    trabalho = input('Você trabalha? [S/N] ').upper()
+    if trabalho == 'S':
+        print('Meus parabéns')
+    else:
+        print('Eu acho bom você começar a trabalhar...')
 else:
     print('Você ainda é menor de idade.')
