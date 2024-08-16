a47b6560c68258b61deab32870ac9718da3fc395
Guilherme <guicseleg@gmail.com>
2024-08-16 20:42:43 -0300

    Melhoria das perguntas
    Criei outra pergunta e melhorei o lugar da outra e outras frases dependentes de cada resposta

--- Codigos-teste/teste.py
+++ Codigos-teste/teste.py
@@ -1,12 +1,23 @@
 nome = input('Digite seu nome: ')
 idade = int(input('Quantos anos você tem? '))
+genero = input('Qual é o seu sexo? [M/F]: ').upper()
+trabalho = input('Você trabalha? [S/N]: ').upper()
 print(f'Que nome bonito o seu {nome}')
-if idade >= 18:
-    print('Você já é maior de idade.')
-    trabalho = input('Você trabalha? [S/N] ').upper()
+if idade == 18:
     if trabalho == 'S':
-        print('Meus parabéns')
+        print('Meus parábens por começar nessa jornada cedo.')
     else:
-        print('Eu acho bom você começar a trabalhar...')
+        print('Bem é melhor começar a pensar sobre o mais rápido possivel.')
+    if genero == 'M':
+        print('Você deve se alistar no exército.')
+    else: 
+        print('Parabéns por se tornar maior de idade, se você quiser você pode se alistar')
+elif 18 < idade < 70:
+    rest = 70 - idade
+    if trabalho == 'S':
+        print(f'Continue assim em {rest} anos você se aposenta.')
+    else:
+        print('Ta esperando o que para procurar um emprego?')
 else:
-    print('Você ainda é menor de idade.')
+    print('Curta a sua aposentadoria!')
+    
